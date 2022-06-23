1cf644c0bf62245b05b4deffaf01265ccfcc1db0
Enside <enside@qq.com>
2022-06-23 15:24:38 +0800

    chore: gitsigns debounce

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,11 +1,7 @@
 require 'gitsigns'.setup {
-  numhl = true,
-  signcolumn = true,
   current_line_blame = true,
   current_line_blame_opts = {
     delay = 300
   },
-  preview_config = {
-    border = 'single'
-  }
+  update_debounce = 300
 }
