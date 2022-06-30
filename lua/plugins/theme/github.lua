a3eb488872154c66bfd82958b8fb988eeb9bf696
Enside <enside@qq.com>
2022-06-30 13:29:20 +0800

    chore: colors

--- lua/plugins/theme/github.lua
+++ lua/plugins/theme/github.lua
@@ -1,13 +1,14 @@
 require 'github-theme'.setup {
   colors = {
+    border = '#4e4e4e',
     bg_visual = '#383d45',
-    border = '#4e4e4e'
+    bg_visual_selection = '#2d3139'
   },
   theme_style = 'dark_default',
   hide_end_of_buffer = false
 }
 
-vim.highlight.create('CmpSelection', { guibg = '#163356' })
+vim.highlight.create('CmpSelection', { guibg = '#2d3139' })
 vim.highlight.create('NvimTreeIndentMarker', { guifg = '#262a33' })
 vim.highlight.create('IndentBlanklineChar', { guifg = '#262a33' })
 vim.highlight.create('IndentBlanklineContextChar', { guifg = '#383d45' })
